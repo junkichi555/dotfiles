887c763d3de1b4f694581c7bab86cae3e44aa3db
matsuo <junkichi0555@gmail.com>
2016-03-13 23:29:54 +0900

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/zsh
 
+# setting for zsh
 cp ./.zprofile $HOME/.zprofile
 cp ./.zshrc $HOME/.zshrc
 cp -r ./.zsh $HOME/.zsh
 cp -r ./.config $HOME/.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/
 
+# setting for vim
 cp ./.vimrc $HOME/.vimrc
 cp -r ./.vim $HOME/.vim
 git clone https://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/
 
+# setting for tmux
 cp ./.tmux.conf $HOME/.tmux.conf
 
+# setting for commands
 cp -r ./bin $HOME/bin
+
+exec $SHELL -l
